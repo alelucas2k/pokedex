45e3889205998a8d25ffd5585f926e0406d80285
Alessandro Lucas <92497473+alelucas2k@users.noreply.github.com>
2023-08-26 00:14:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMore = document.getElementById('loadMore')
+
+//qtd de pokemons da primeira geração
+const maxRecords = 151
+//parametros de controle da quantidade de pokemons puxados na api
+const limit = 10
+let offset = 0
+
+
+function loadmoreitens(offset, limit){
+
+    pokeapi.getPokemons(offset, limit).then((pokemons = []) => (
+        // concatenando e transformando a lista de pokemons em html
+        pokemonList.innerHTML += pokemons.map((pokemon) => ` 
+                <li class="pokemon ${pokemon.type}">
+                    <span class="number">#${pokemon.number} </span>
+                    <span class="name"> ${pokemon.name}</span>
+                    
+                    <div class="details">
+                            <ol class="types">
+                                ${pokemon.types.map((type) => `<li class="type ${type}" > ${type}</li>`).join(' ')}
+                            </ol>
+        
+                        <img src="${pokemon.photo}" alt="${pokemon.name}">
+                    </div>
+                </li>
+        `).join('')
+    ))
+}
+
+loadmoreitens(offset, limit)
+
+// função p esconder o botão de more quando chegar no limit pre-definido
+loadMore.addEventListener('click', () => {
+    offset += limit
+    Recordwnxtpage = offset + limit 
+
+    if(Recordwnxtpage >= maxRecords){
+        newLimit = (maxRecords - offset)
+        loadmoreitens(offset, newLimit)
+
+        loadMore.parentElement.removeChild(loadMore)
+    } else {
+        loadmoreitens(offset, limit)
+    }
+})
---
+++ poke-api.js
@@ -0,0 +1,44 @@
+const pokeapi = {}
+
+function pokeapiTopokemon(pokemonDetails){
+
+    const pokemon = new Pokemon()
+    pokemon.name = pokemonDetails.name
+    pokemon.number = pokemonDetails.id
+
+    const types = pokemonDetails.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+    pokemon.photo = pokemonDetails.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+pokeapi.getpokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+    .then(pokeapiTopokemon)
+}
+
+pokeapi.getPokemons = (offset=0, limit=10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map((pokeapi.getpokemonDetail)))
+        .then((pokemonDetails) => Promise.all(pokemonDetails))
+        .then((pokemonDetails) => pokemonDetails)
+
+
+
+        .catch((error) => console.error(error))
+
+}
+
+Promise.all([
+
+]).then((results) => {
+    console.log(results)
+})
